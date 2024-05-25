c758ec111702d20cf452e1d5c7272fe2792d7728
Matheus Torres <matheustf2003@gmail.com>
2024-05-25 16:48:55 -0300

    Initial Commit

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by Matheus Torres on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
